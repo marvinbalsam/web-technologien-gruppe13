7607a3577524e90c10d7d086853fa1a925fbadc6
gabrielxxxxxxxx <gabriel.haardt@std.dhsh.de>
2024-05-23 09:41:48 +0200

    Kontaktformular erstellt

--- contact.php
+++ contact.php
@@ -10,7 +10,24 @@ include 'navigation.php';
 </head>
 <body>
 
-<h1>Webshop</h1>
+<header>
+        <h1>Kontaktieren Sie uns</h1>
+    </header>
+    <div class="container">
+        <form action="submit_form.php" method="post">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+            <br>
+
+            <label for="email">E-Mail:</label>
+            <input type="email" id="email" name="email" required>
+            <br>
+            <label for="message">Nachricht:</label>
+            <textarea id="message" name="message" rows="5" required></textarea>
+            <br>
+            <input type="submit" value="Absenden">
+        </form>
+    </div>
 
 
 </body>
