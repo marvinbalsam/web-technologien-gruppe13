5e57b2db18870fbd39fa55e271a4af7d6608c8ac
gabrielxxxxxxxx <gabriel.haardt@std.dhsh.de>
2024-06-12 11:38:15 +0200

    Logik und SQL Statement für Registrierung hinzugefügt

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ include 'dbconnect.php';
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Webshop</title>
+    <title>Registrierung - Webshop</title>
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./style.css">
 </head>
@@ -13,15 +13,26 @@ include 'dbconnect.php';
 
 <h1>Webshop</h1>
 <h2>Bitte registrieren Sie sich mit Ihren persönlichen Daten.</h2>
-
-<form>
+<?php
+if(isset($_GET['register'])){
+  $error = false;
+  $firstname = $_POST['firstname'];
+  $name = $_POST['name'];
+  $mail = $_POST['mail'];
+  $password = $_POST['password'];
+  $passwordhash = password_hash($password, PASSWORD_DEFAULT);
+  $statement = mysqli_query($link, "INSERT INTO user (firstname, name, mail, password) VALUES ('$firstname', '$name', '$mail', '$passwordhash')");
+  echo("erfolgreich!");
+}
+?>
+<form action="?register=1" method="post">
 
   <label for="firstname">Vorname:</label><br>
   <input type="text" id="firstname" name="firstname"><br>
   <label for="name">Name:</label><br>
   <input type="text" id="name" name="name"><br>
   <label for="name">E-Mail:</label><br>
-  <input type="text" id="mail" name="mail"><br>
+  <input type="email" id="mail" name="mail"><br>
   <label for="password">Passwort:</label><br>
   <input type="password" id="password" name="password"><br><br>
   <input type="submit" value="Registrieren">
