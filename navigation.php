973be07bbcd2574254ce3ff12eb9ffe693ab4caa
Phillip <phillipkelsch9813@gmail.com>
2024-06-19 02:53:49 +0200

    änderung datenbankzugriff auf vorherige Variante, Grundlagen Shopping Cart

--- navigation.php
+++ navigation.php
@@ -4,4 +4,5 @@
   <a href="contact.php">Kontakt</a>
   <a href="login.php">Login</a>
   <a href="register.php">Registrierung</a>
+  <a id="cart" class="active left" href ="#cart">Warenkorb (<?php $cartItemsCount?>)</a>
 </div>
--- products.php
+++ products.php
@@ -2,13 +2,17 @@
 include 'navigation.php';
 include 'dbconnect.php';
 
-$username = "root";
-$password = "";
-$dsn = "mysql:host=127.0.0.1;dbname=db_webshop;charset=utf8";
-$db = new PDO($dsn, $username, $password);
+$sql1 = "SELECT product_id, name, description, price, image FROM products";
+$result = mysqli_query($link, $sql1);
 
-$sql = "SELECT product_id, name, description, price, image FROM products";
-$result = $db->query($sql);
+$userId = 123;
+$cartItemsCount = 0;
+
+
+$sql2 = "SELECT COUNT(product_id) FROM cart WHERE user_id = ".$userId;
+$cartResult = mysqli_query($link, $sql2);
+$cartItemsCount = mysqli_fetch_column($cartResult);
+
 
 ?>
 <!DOCTYPE html>
@@ -18,7 +22,7 @@ $result = $db->query($sql);
     <title>Webshop</title>
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
@@ -31,7 +35,7 @@ $result = $db->query($sql);
         </header>
         <section class="container" id="products">
             <div class="row">
-                <?php while($row = $result->fetch()):?> 
+                <?php while($row = mysqli_fetch_assoc($result)):?> 
                 <div class="col">
                   <?php include 'card.php'?>
                 </div>
